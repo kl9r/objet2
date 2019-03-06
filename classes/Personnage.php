e46fc9af8bacc7081c4de211f106c7602d404cbe
Emmanuel MOUTIER <emmanuelmoutier@gmail.com>
2019-03-06 10:45:40 +0100

    creation de la class personnage

---
+++ classes/Personnage.php
@@ -0,0 +1,26 @@
+<?php
+
+class personnage
+{
+    public $nom;
+    public $vie = 100;
+    public $attaque;
+    public $taille;
+    public $masse;
+    public $race;
+
+    public function marcher() : string
+    {
+        return "Je marche !";
+    }
+
+    public function recupererPointDeVie() : int
+    {
+        return $this->vie;
+    }
+
+
+
+
+
+}
